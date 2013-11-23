1d770ce776898f9836871d3203eb4b12d2e734e0
yong.teng <webmaster@buession.com>
2013-11-23 15:54:39 +0800

    fix
    Change-Id: I635206838841cceb5f2321b206ae64b021d1e5cf

--- buession.c
+++ buession.c
@@ -165,8 +165,6 @@ static ZEND_METHOD(buession, getInstance){
 			}
 		}
 
-		php_printf("\r\nclassname: %s\r\n", package);
-		return;
 		if(zend_lookup_class_ex(classname, classname_length, FALSE, &ce TSRMLS_CC) == SUCCESS){
 			buession_free(classname);
 			instance = buession_getInstance(*ce, NULL, argc, args TSRMLS_CC);
